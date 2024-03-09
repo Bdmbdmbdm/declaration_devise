ecd3228d36281d174469fb1107ad7b1a9277521e
bdmbdmbdm <44704171+mahabdh@users.noreply.github.com>
2024-03-09 22:56:25 +0100

    Update espace_voyageur.php

--- declaration/declaration/espace_voyageur.php
+++ declaration/declaration/espace_voyageur.php
@@ -1,59 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="icon" type="image/png" href="a68-douanes-algerienne.jpg"/>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-	<meta charset="utf-8">
-	<title>
-		Espace Voyageur
-	</title>
-</head>
-<body>
-	<style type="text/css">
-		body {
-		  background: url('images/2014_DOUANE_261536149.jpg') no-repeat center center fixed;
-		  -webkit-background-size: cover;
-		  -moz-background-size: cover;
-		  -o-background-size: cover;
-		  background-size: cover;
-		}
-
-		.panel-default {
-		  opacity: 0.9;
-		  margin-top:30px;
-		}
-		.form-group.last { margin-bottom:0px; }t { margin-bottom:0px; }
-		.container{
-			margin-top: 0;
-			margin-bottom: 0 !important;
-		}
-	</style>
-	<div class="container">
-		<div class="panel panel-default">
-		<div class="panel-body">
-			<p align="center"><b> الجمهــــــوريـــــــــة الجــزائريـــــــــة الـديمقراطــــيــــــة الشعبـــــيـــــــة </b></p>
-		<p align="center"><b> REPUBLIQUE ALGERIENNE DEMOCRATIQUE ET POPULAIRE </b></p>
-		<table border="0" align="center">
-			<tr>
-				<td width="30%">
-					<p align="center" style="font-size: 15px;"><b>MINISTERE DES FINANCES</b></p>
-				    <hr align="center" width="80px" color="black">
-					<p align="center"  ><b>DIRECTION GENERALE DES DOUANES</b></p>
-					<hr align="center" width="80px" color="black">
-				</td>
-				<td width="30%">
-					<p align="center"><img src="douane.jpg"></p>
-				</td>
-				<td width="30%">
-					<p align="center"><b>وزارة المــــاليــــــــة</b></p>
-						<hr align="center" width="80px" color="black">
-						<p align="center"><b>المديريــة العامــة للجمــــارك</b></p>
-						<hr align="center" width="80px" color="black">
-				</td>
-			</tr>
-		</table>
+	<lin
 		</div>
 	</div>
 </div>
